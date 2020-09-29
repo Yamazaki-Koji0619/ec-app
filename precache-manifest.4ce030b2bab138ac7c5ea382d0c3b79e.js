edc1e3795a3a9612370add50a3f377c5313baf9a
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2020-09-29 23:25:51 +0900

    Updates

---
+++ precache-manifest.4ce030b2bab138ac7c5ea382d0c3b79e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa9e00f05b93690ba1c7b0014aa5e3e",
+    "url": "/ec-app/index.html"
+  },
+  {
+    "revision": "f49321785cb9371cba96",
+    "url": "/ec-app/static/css/2.67246356.chunk.css"
+  },
+  {
+    "revision": "e257f1f11ebb8924187d",
+    "url": "/ec-app/static/css/main.086e54c7.chunk.css"
+  },
+  {
+    "revision": "f49321785cb9371cba96",
+    "url": "/ec-app/static/js/2.a89b52b2.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/ec-app/static/js/2.a89b52b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e257f1f11ebb8924187d",
+    "url": "/ec-app/static/js/main.e985974f.chunk.js"
+  },
+  {
+    "revision": "af3bae9248c481320306",
+    "url": "/ec-app/static/js/runtime-main.b72132a1.js"
+  },
+  {
+    "revision": "a953fa1653ea3b3a478f2b25a607dc58",
+    "url": "/ec-app/static/media/no_image.a953fa16.png"
+  }
+]);
