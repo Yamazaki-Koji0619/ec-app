b8175927f45ba5df9b4e1ee59a21580a1f8d61f6
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2020-12-22 14:04:13 +0900

    add test

--- src/components/Products/SetLevel.jsx
+++ src/components/Products/SetLevel.jsx
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 
 const SetLevel = (props) => {
 
-    const [level, setLevel] = useState('');
+    console.log(props)
 
-    console.log(level);
+    const [DifficultyLevel, setDifficultyLevel] = useState("");
+    let level = "";
 
     const DisplayLevel = () => {
+
         if(props.level === 'easy'){
-            setLevel('簡単');
+            level = "簡単";
+            console.log(level);
+            setDifficultyLevel(level);
             
             return <div className="s-display-flex">
                         <div className="s-text-level s-easy_color">easy</div>
-                        <div className="s-text-level"></div>
                    </div>
         }else if(props.level === 'little easy'){
-            setLevel('少し簡単');
+            level = "少し簡単";
 
             return <div className="s-display-flex">
                         <div className="s-text-level s-little-easy_color">little easy</div>
                         <div className="s-text-level s-little-easy_color"></div>
                    </div>
         }else if(props.level === 'normal'){
-            setLevel('普通');
+            level = "普通";
 
             return <div className="s-display-flex">
                         <div className="s-text-level s-normal_color">normal</div>
@@ -30,7 +33,7 @@ const SetLevel = (props) => {
                         <div className="s-text-level s-normal_color"></div>
                    </div>
         }else if(props.level === 'little difficult'){
-            setLevel('少し難しい');
+            level = "少し難しい";
 
             return <div className="s-display-flex">
                         <div className="s-text-level s-little-diff_color">little-diff</div>
@@ -39,7 +42,7 @@ const SetLevel = (props) => {
                         <div className="s-text-level s-little-diff_color"></div>
                    </div>
         }else if(props.level === 'difficult'){
-            setLevel('難しい');
+            level = "難しい";
 
             return <div className="s-display-flex">
                         <div className="s-text-level s-diff_color">difficult</div>
@@ -53,7 +56,7 @@ const SetLevel = (props) => {
 
     return(
         <>
-            <div>着こなしやすさ：{level}</div>
+            <div>着こなしやすさ：{DifficultyLevel}</div>
             {DisplayLevel()}
         </>
     )
