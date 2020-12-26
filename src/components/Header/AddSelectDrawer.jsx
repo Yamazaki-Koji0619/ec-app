f508a4e258ee7fe2e526fdcf8e047e5d86593fe9
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2020-12-26 21:59:46 +0900

    create AddSelectDrwer

--- src/components/Header/AddSelectDrawer.jsx
+++ src/components/Header/AddSelectDrawer.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/styles';
+import { db } from '../../firebase/index';
 
 const useStyles = makeStyles((theme) => ({
     drawer: {
@@ -25,6 +26,30 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const AddSelectDrwer = (props) => {
+
+    const type = props.type;
+    const [detailItem, setDetailItem] = useState([]);
+
+    const selectMenu = (event, path) => {
+        dispatch(push(path))
+        props.onClose(event)
+    };
+
+    useEffect(() => {
+        db.collection(type).orderBy('order', 'asc').get()
+        .then(snapshots => {
+            const list = [];
+            snapshots.forEach(snapshot => {
+                const data = snapshot.data();
+                list.push(
+                    {func: selectMenu, label: data.name, id: data.id, value: `/ec-app/?${type}=${data.id}`},
+                )
+            })
+            setDetailItem(() => [...list]);
+            console.log(detailItem);
+        })
+    }, []);
+
     return(
         <>
             <div>AddSelectDrwer</div>
@@ -32,12 +57,18 @@ const AddSelectDrwer = (props) => {
                 container={container}
                 variant="temporary"
                 anchor="right"
-                open={props.open}
+                open={props.detail}
                 onClose={(e) => {props.onClose(e)}}
                 classes={{paper: classes.drawerPaper}}
                 ModalProps={{keepMounted: true}}
             >
-                <List></List>
+                <List>
+                    {detailItem.map(item => (
+                        <ListItem button key={item.id} onClick={(e) => item.func(e, item.value)}>
+                            <ListItemText primary={item.label} />
+                        </ListItem>
+                    ))}
+                </List>
             </Drawer>
         </>
     )
