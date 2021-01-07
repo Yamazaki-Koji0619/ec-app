e702ee9225e3e03dae66258804011397a787fc14
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2021-01-07 15:59:18 +0900

    Updates

---
+++ precache-manifest.daf262d3528eb02ca7fe5b81a50690fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0440b11c24c5b268a1c18d7a33ebca3d",
+    "url": "/ec-app/index.html"
+  },
+  {
+    "revision": "423722a6f75f86d1c7d7",
+    "url": "/ec-app/static/css/2.67246356.chunk.css"
+  },
+  {
+    "revision": "572b82add6ce5289dfeb",
+    "url": "/ec-app/static/css/main.52bc2aa9.chunk.css"
+  },
+  {
+    "revision": "423722a6f75f86d1c7d7",
+    "url": "/ec-app/static/js/2.ac522b02.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/ec-app/static/js/2.ac522b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572b82add6ce5289dfeb",
+    "url": "/ec-app/static/js/main.0eb29c2c.chunk.js"
+  },
+  {
+    "revision": "af3bae9248c481320306",
+    "url": "/ec-app/static/js/runtime-main.b72132a1.js"
+  },
+  {
+    "revision": "32b21d3a8cf1ecb991de7704c50ba4b3",
+    "url": "/ec-app/static/media/man.32b21d3a.png"
+  },
+  {
+    "revision": "17944139c0ddd48e84cf9ec4a02b226c",
+    "url": "/ec-app/static/media/man_woman.17944139.png"
+  },
+  {
+    "revision": "a953fa1653ea3b3a478f2b25a607dc58",
+    "url": "/ec-app/static/media/no_image.a953fa16.png"
+  },
+  {
+    "revision": "559d1d892af359586662d2c961c7542b",
+    "url": "/ec-app/static/media/woman.559d1d89.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ec-app/precache-manifest.b76dca147cec3b91b02650547923e8c7.js"
+  "/ec-app/precache-manifest.daf262d3528eb02ca7fe5b81a50690fe.js"
 );
 
 self.addEventListener('message', (event) => {
